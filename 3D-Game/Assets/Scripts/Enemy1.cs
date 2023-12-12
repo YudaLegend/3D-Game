583f4c34253fcf5b5678b70ac47a459ca3222e21
jinheng.lin <jinheng.lin@estudiantat.upc.edu>
2023-12-12 11:50:31 +0100

    something

--- 3D-Game/Assets/Scripts/Enemy1.cs
+++ 3D-Game/Assets/Scripts/Enemy1.cs
@@ -1,28 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Enemy1 : MonoBehaviour
 {
     public float rotationSpeed, jumpSpeed, gravity;
-
+    public float health;
+    public float shield;
+    public float maxHealth;
+    public float maxSheild;
+    
+    public GameObject healthBarUI;
+    public Slider s;
+    public Slider h;
     Vector3 startDirection;
-    public float speedY;
-
-    bool cicle; // false -> outern 
-    bool changeButton;
-    public bool inmortal;
-    bool changeInmortal;
     // Start is called before the first frame update
     public bool dir;
-
-    public float dashTimer;
-    bool changeDash;
-
-    public int vida;
-    public float dashEnergy;
-
-    public bool bigJump;
     void Start()
     {
         // Store starting direction of the player with respect to the axis of the level
@@ -30,41 +23,25 @@ public class Enemy1 : MonoBehaviour
         startDirection.y = 0.0f;
         startDirection.Normalize();
         dir = false;
-        speedY = 0;
-        cicle = false;
-        changeButton = false;
-        inmortal = false;
-        changeInmortal = false;
-        dashTimer = 0;
-        changeDash = false;
-        vida = 5;
-        dashEnergy = 0.0f;
-        bigJump = false;
+        health = maxHealth;
+        shield = maxSheild;
+        s.value = shield/maxSheild;
+        h.value = health/maxHealth;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        s.value = shield/maxSheild;
+        h.value = health/maxHealth;
+        if(health <= 0){
+            Destroy(gameObject);
+        }
+        if(health > maxHealth){
+            health = maxHealth;
+        }
         CharacterController charControl = GetComponent<CharacterController>();
-        Vector3 position;
-        // Left-right movement
-        if (dashTimer >= 0){
-            inmortal = true;
-            Dashing();
-        }
-        else if (Input.GetKey(KeyCode.E) && !changeDash){
-            dashTimer = 0.1f;
-            changeDash = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.E) && changeDash){
-            changeDash = false;
-        }
-        else if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && dashTimer < 0)
-        {
-            inmortal = false;
-            Moving();
-        }
-
+        Moving();
         // Correct orientation of player
         // Compute current direction
         Vector3 currentDirection = transform.position - transform.parent.position;
@@ -79,65 +56,8 @@ public class Enemy1 : MonoBehaviour
         else
             orientation = Quaternion.FromToRotation(startDirection, currentDirection);
         transform.rotation = orientation;
-
-        // Apply up-down movement
-        position = transform.position;
-        if (charControl.Move(speedY * Time.deltaTime * Vector3.up) != CollisionFlags.None)
-        {
-            transform.position = position;
-            Physics.SyncTransforms();
-        }
-        if (charControl.isGrounded)
-        {
-            if(bigJump && jumpSpeed != 10){
-                jumpSpeed = 10;
-            }else if(!bigJump && jumpSpeed != 5){
-                jumpSpeed = 5;
-            }
-            if (speedY < 0.0f)
-                speedY = 0.0f;
-            if (Input.GetKey(KeyCode.W))
-                speedY = jumpSpeed;
-        }
-        else
-            speedY -= gravity * Time.deltaTime;
-
-        if (Input.GetKey(KeyCode.Q) && !changeButton){
-            changeButton = true;
-            cicle = !cicle;
-            float angle = Mathf.Atan2(transform.position.x, transform.position.z);
-            if(cicle){
-                //intern
-                Vector3 pos = transform.position;
-                pos.x = (float)1.84*Mathf.Sin(angle);
-                pos.z = (float)1.84*Mathf.Cos(angle);
-                pos.y = (float)(pos.y + 0.38);
-                transform.position = pos;
-            }else {
-                //outern
-                Vector3 pos = transform.position;
-                pos.x = (float)2.91*Mathf.Sin(angle);
-                pos.z = (float)2.91*Mathf.Cos(angle);
-                pos.y = (float)(pos.y - 0.37);
-                transform.position = pos;
-            }
-        }else if(Input.GetKeyUp(KeyCode.Q) && changeButton){
-            changeButton = false;
-        }
-
-        if (Input.GetKey(KeyCode.R) && !changeInmortal){
-            changeInmortal = true;
-        }else if(Input.GetKeyUp(KeyCode.R) && changeInmortal){
-            changeInmortal = false;
-            inmortal = !inmortal;
-        }
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Entered collision with " + collision.gameObject.name);
-        bigJump = true;
-    }
 
     /// <summary>
     /// OnTriggerEnter is called when the Collider other enters the trigger.
@@ -146,15 +66,19 @@ public class Enemy1 : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Entered collision with " + other.gameObject.name);
-        if(other.gameObject.tag == "Jumper")
-            bigJump = true;
+        if(other.gameObject.tag == "Obstacle"){
+            dir = !dir;
+        }
     }
 
-    void OnTriggerExit(Collider other)
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter(Collision other)
     {
-        Debug.Log("Exited collision with " + other.gameObject.name);
-        if(other.gameObject.tag == "Jumper")
-            bigJump = false;
+        Debug.Log("Entered collision with " + other.gameObject.name);
     }
 
     void Moving(){
@@ -166,38 +90,6 @@ public class Enemy1 : MonoBehaviour
         position = transform.position;
         angle = rotationSpeed * Time.deltaTime;
         direction = position - transform.parent.position;
-        if (Input.GetKey(KeyCode.A))
-        {
-            target = transform.parent.position + Quaternion.AngleAxis(angle, Vector3.up) * direction;
-            if (charControl.Move(target - position) != CollisionFlags.None)
-            {
-                transform.position = position;
-                Physics.SyncTransforms();
-            }
-            dir = false;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            target = transform.parent.position + Quaternion.AngleAxis(-angle, Vector3.up) * direction;
-            if (charControl.Move(target - position) != CollisionFlags.None)
-            {
-                transform.position = position;
-                Physics.SyncTransforms();
-            }
-            dir = true;
-        }
-    }
-    
-    void Dashing(){
-        dashTimer -= Time.deltaTime;
-        CharacterController charControl = GetComponent<CharacterController>();
-        Vector3 position;
-        float angle;
-        Vector3 direction, target;
-        rotationSpeed = 500;
-        position = transform.position;
-        angle = rotationSpeed * Time.deltaTime;
-        direction = position - transform.parent.position;
         if (!dir)
         {
             target = transform.parent.position + Quaternion.AngleAxis(angle, Vector3.up) * direction;
@@ -206,9 +98,8 @@ public class Enemy1 : MonoBehaviour
                 transform.position = position;
                 Physics.SyncTransforms();
             }
-            dir = false;
         }
-        else if (dir)
+        if (dir)
         {
             target = transform.parent.position + Quaternion.AngleAxis(-angle, Vector3.up) * direction;
             if (charControl.Move(target - position) != CollisionFlags.None)
@@ -216,9 +107,7 @@ public class Enemy1 : MonoBehaviour
                 transform.position = position;
                 Physics.SyncTransforms();
             }
-            dir = true;
         }
-
     }
 
 }
